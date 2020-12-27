ad5bb4d8e32a856c1b9af5618dc2139334303cb6
linoferreira <linoafferreira@gmail.com>
2020-12-27 19:20:06 +0000

    Add aesthetics from N. Rougier, tramp, fixed width fonts, clean
    Changes:
    - variable-pitch font options move to archive at the end of
    the file
    - theme changes from Doom One Light to Solarized Light
    - smart parentheses removed move to archive
    - change file paths from MEGA to Tresorit
    - remove custom TODO labels
    - add tramp
    - add flycheck
    - move super-agenda and lsp-mode to archive

--- init.el
+++ init.el
@@ -23,16 +23,40 @@
 ;; disable toolbar, menu bar and scroll bar
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(toggle-scroll-bar -1) 
+(toggle-scroll-bar -1)
+
+;; remove initial scratch message
+(setq initial-scratch-message nil)
+
+;; left margin
+(set-window-margins nil 2)
+
+;; borrowed from Nicolas Rougier's EMACS / NANO
+;; No line breat space points
+(setq auto-fill-mode nil)
+;; Fill column at 80
+(setq fill-column 80)
+;; Pixel scroll (as opposed to char scrool)
+;; (pixel-scroll-mode t)
+;; No tabs
+(setq-default indent-tabs-mode nil)
+;; Tab.space equivalence
+(setq tab-width 4)
+;; Buffer encoding
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment   'utf-8)
 
 ;; fonts
 ;; set default font
 (set-frame-font "Source Code Pro-10")
 ;; set variable-width font
 (set-face-font 'variable-pitch "Source Sans Pro-12")
-;; choose when to use variable-width font smartly
-(use-package mixed-pitch
-  :hook (text-mode . mixed-pitch-mode))
+
+;; increase line spacing
+(setq-default line-spacing 0.1)
 
 ;; icons
 (use-package all-the-icons)
@@ -42,7 +66,7 @@
 ;; theme
 (use-package doom-themes
   :config
-  (load-theme 'doom-one-light t)  ; theme
+  (load-theme 'doom-solarized-light t)  ; theme
   (doom-themes-visual-bell-config)  ; enable flashing mode line on errors
   (setq doom-themes-treemacs-theme "doom-colors")  ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
@@ -71,7 +95,8 @@
 
 
 
-;; auto-completion ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; company-mode (in-buffer code completion)
+;; auto-completion ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; company-mode (in-buffer code completion)
 (use-package company
   :init (global-company-mode t))
 (use-package company-auctex)
@@ -87,7 +112,7 @@
 
 
 
-;; misc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; misc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; evil mode (vim keybindings)
 (use-package evil
   :config (evil-mode 1)
@@ -114,20 +139,6 @@
   (which-key-mode)
   (setq which-key-allow-evil-operators t))
 
-;; highlight matching parentheses (TODO simplify this config!)
-;; see https://notabug.org/stefano-m/.emacs.d/src/84a0a380d943ebe1627b5f63fb4d5aec681ae81d/init.d/parens.cfg.el
-;; for smartparens-config
-(use-package smartparens-config
-  :ensure smartparens
-  :init
-  (add-hook 'minibuffer-setup-hook #'turn-on-smartparens-strict-mode)
-  :config
-  (show-smartparens-global-mode t)
-  (smartparens-global-mode t)
-  (sp-use-smartparens-bindings)
-  (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-  :diminish smartparens-mode)
-
 ;; set file to store customise options (create it if it doesn't exist)
 (unless (file-exists-p "~/.emacs.d/custom.el")
   (write-region "" nil "~/.emacs.d/custom.el"))
@@ -140,45 +151,48 @@
 
 
 
+
 ;; notes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org-mode
 (use-package org
   :hook
-  ;; (org-mode . org-indent-mode)
+  ;; (org-mode . variable-pitch-mode)
+  (org-mode . org-indent-mode)
   (org-mode . visual-line-mode)
-  ;; (org-mode . flyspell-mode)
-  (org-mode . variable-pitch-mode)
+  (org-mode . flyspell-mode)
   :bind
   ("C-c a" . org-agenda)
   ("C-c c" . org-capture)
   :config
-  (setq org-agenda-files (quote ("~/MEGA/notes"))  ; org-agenda dirs 
+  (setq org-agenda-files (quote ("~/Tresorit/notes"))  ; org-agenda dirs 
   org-hide-emphasis-markers t  ; hide emphasis markers
   org-format-latex-options (plist-put org-format-latex-options :scale 1.5)  ; LaTeX preview: increase font size
+  org-archive-location "~/Tresorit/notes/archive.org::* From %s"  ; archive file
+  ;; org-todo-keywords '((sequence "TODO" "NEXT" "|" "DONE"))
+  ;; org-todo-state-tags-triggers '(("NEXT" ("ARCHIVE" . t)))
   ;; org-capture
   org-capture-templates
-  '(("r" "Research" entry (file+headline "~/MEGA/notes/research.org" "Capture")
+  '(("r" "Research" entry (file+headline "~/Tresorit/notes/research.org" "Capture")
      "** %U %?\n")
-    ("p" "Personal" entry (file+headline "~/MEGA/notes/personal.org" "Capture")
+    ("p" "Personal" entry (file+headline "~/Tresorit/notes/personal.org" "Capture")
      "** %U %?\n"))))
 
-;; UTF-8 bullet points
+;; UTF-8 bullet points for org-mode
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode))
 
-;; no distractions mode
+;; centred text mode
 (use-package olivetti
   :bind ("C-c o" . olivetti-mode))
+(setq-default olivetti-body-width 100)
 
-
-
-;; reference management ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; reference management
 ;; ivy-bibtex (use ivy completion framework for searching references)
 (use-package ivy-bibtex
   :config
-  (setq bibtex-completion-bibliography '("~/MEGA/gms/library/zotero.bib")
+  (setq bibtex-completion-bibliography '("~/Tresorit/gms/library/zotero.bib")
         bibtex-completion-pdf-field "file"
-        bibtex-completion-notes-path "~/MEGA/notes/papers/"
+        bibtex-completion-notes-path "~/Tresorit/notes/papers/"
         bibtex-completion-pdf-open-function  ;; open PDFs with system viewer
 	(lambda (fpath)
 	  (call-process "evince" nil 0 nil fpath))))
@@ -186,7 +200,93 @@
 ;; org-ref (easily add references to org files)
 (use-package org-ref
   :config
-  (setq reftex-default-bibliography '("~/MEGA/gms/library/zotero.bib")
-	org-ref-default-bibliography '("~/MEGA/gms/library/zotero.bib")
-	org-ref-bibliography-notes "~/MEGA/notes/papers/"
+  (setq reftex-default-bibliography '("~/Tresorit/gms/library/zotero.bib")
+	org-ref-default-bibliography '("~/Tresorit/gms/library/zotero.bib")
+	org-ref-bibliography-notes "~/Tresorit/notes/papers/"
 	org-ref-completion-library 'org-ref-ivy-bibtex))
+
+;; tramp
+(use-package tramp
+  :defer t
+  :config
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+;; (add-to-list 'tramp-remote-path "/apps/eb/skylake/software/git/2.23.0-GCCcore-8.3.0-nodocs/bin")
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; flycheck
+(use-package flycheck
+  :ensure t)
+  ;; :init (global-flycheck-mode)
+
+
+
+
+;; archive ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; variable width font config
+;; (let* ((variable-tuple
+;;         (cond ((x-list-fonts "Noto Serif") '(:font "Noto Serif"))))
+;;        (base-font-color     (face-foreground 'default nil 'default))
+;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+;;   (custom-theme-set-faces
+;;    'user
+;;    '(variable-pitch ((t (:family "Noto Serif" :height 110))))
+;;    '(fixed-pitch ((t ( :family "Source Code Pro" :height 100))))
+;;    '(org-block ((t (:inherit fixed-pitch))))
+;;    '(org-block-begin-line ((t (:inherit fixed-pitch))))
+;;    '(org-block-end-line ((t (:inherit fixed-pitch))))
+;;    '(org-code ((t (:inherit (shadow fixed-pitch)))))
+;;    ;; '(org-document-info ((t (:foreground "dark orange"))))
+;;    ;; '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+;;    '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+;;    ;; '(org-link ((t (:foreground "royal blue" :underline t))))
+;;    ;; '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+;;    ;; '(org-property-value ((t (:inherit fixed-pitch))) t)
+;;    ;; '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+;;    ;; '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+;;    '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+;;    '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+;;    ))
+
+;; choose when to use variable-width font smartly
+;; (use-package mixed-pitch
+;;   :hook (text-mode . mixed-pitch-mode))
+
+
+;; highlight matching parentheses
+;; see https://notabug.org/stefano-m/.emacs.d/src/84a0a380d943ebe1627b5f63fb4d5aec681ae81d/init.d/parens.cfg.el
+;; for smartparens-config
+;; (use-package smartparens-config
+;;   :ensure smartparens
+;;   :init
+;;   (add-hook 'minibuffer-setup-hook #'turn-on-smartparens-strict-mode)
+;;   :config
+;;   (show-smartparens-global-mode t)
+;;   (smartparens-global-mode t)
+;;   (sp-use-smartparens-bindings)
+;;   (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+;;   :diminish smartparens-mode)
+ 
+
+;; super-agenda
+;; (use-package org-super-agenda
+;;   :config
+;;   (setq org-super-agenda-groups
+;;        '(;; Each group has an implicit boolean OR operator between its selectors.
+;;          (:name "Today"  ; Optionally specify section name
+;;                 :time-grid t)  ; Items that appear on the time grid 
+;;          (:name "PRS portability project"
+;;                 :tag "PRS")
+;;          (:name "IBD recombination project"
+;;                 :tag "IBD")
+;;          (:name "Admin"
+;;                 :tag "admin")))
+;;   (org-agenda nil "a"))
+
+
+;; lsp
+;; (use-package lsp-mode
+;;     :hook ( (ess-r-mode-hook . lsp)
+;;             (lsp-mode . lsp-enable-which-key-integration))
+;;     :config
+;;     (setq lsp-clients-r-server-command '("R" "--slave" "-e" "languageserver::run()")) )
