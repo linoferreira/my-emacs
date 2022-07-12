09bd29b76a1ef697bc843e9e3f9568572b7a7342
linoferreira <linoafferreira@gmail.com>
2022-07-12 17:40:52 +0100

    Get Tramp working on BMRC with 2FA and tab-completion in shell
    Other changes:
    - increase font size
    - key binding for assignment operator <- in ESS
    - allow hash to be entered in macOS
    - key binding to toggle truncate-lines
    - minor org-mode changes
    - update org-ref settings

--- init.el
+++ init.el
@@ -19,6 +19,12 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
+;; correct paths (same as shell)
+(unless (package-installed-p `exec-path-from-shell)
+  (package-refresh-contents)
+  (package-install `exec-path-from-shell))
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
 
 ;; aesthetics ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -59,7 +65,7 @@
 ;; fonts
 ;; set default font
 ;; (set-frame-font "Source Code Pro-10")
-(set-frame-font "Roboto Mono-9")
+(set-frame-font "Roboto Mono-11")
 ;; (set-face-font 'italic "Roboto Mono Italic")
 ;; set variable-width font
 ;; (set-face-font 'variable-pitch "Source Sans Pro-12"
@@ -99,7 +105,11 @@
 ;; support for languages ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; R
 (use-package ess
-  :defer t)
+  :defer t
+  :config
+  ;; key binding for assignment operator <-
+  (eval-after-load "ess-mode" '(define-key ess-mode-map (kbd "C-'") "<-"))
+  (eval-after-load "ess-mode" '(define-key inferior-ess-mode-map (kbd "C-'") "<-")))
 
 ;; LaTeX (TODO add spell checking by default!)
 (use-package auctex
@@ -149,6 +159,9 @@
   :config
   (evil-collection-init))
 
+;; allow hash to be entered (see https://stackoverflow.com/questions/3977069/emacs-question-hash-key)
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+
 ;; git integration
 (use-package magit
   :bind ("C-x g" . magit-status))
@@ -183,6 +196,21 @@
   (make-directory "~/.emacs.d/backups/" t))
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
 
+;; key binding for toggling truncate lines
+(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+
+;; open Snakemake files in python-mode
+(add-to-list 'auto-mode-alist '("\\Snakefile\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.smk\\'" . python-mode))
+
+;; tab completion in shell-mode
+(use-package bash-completion
+  :config
+  (autoload 'bash-completion-dynamic-complete
+    "bash-completion"
+    "BASH completion hook")
+  (add-hook 'shell-dynamic-complete-functions
+            'bash-completion-dynamic-complete))
 
 
 
@@ -199,15 +227,18 @@
   ("C-c c" . org-capture)
   :config
   (setq org-agenda-files (quote ("~/Tresorit/notes/" "~/Tresorit/notes/projects/"))  ; org-agenda dirs
-        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)"))
+        org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "CANCELLED(c)"))
         org-log-done 'time
         org-hide-emphasis-markers t  ; hide emphasis markers
         org-format-latex-options (plist-put org-format-latex-options :scale 1.5)  ; LaTeX preview: increase font size
         org-archive-location "~/Tresorit/notes/archive.org::* From %s"  ; archive file
-        org-capture-templates '(("r" "Research" entry (file+headline "~/Tresorit/notes/dphil.org" "Capture")
+        org-capture-templates '(("i" "Inbox" entry (file "~/Tresorit/notes/inbox.org")
+                                 "* %?\n :PROPERTIES:\n :Date of notes: %U\n :END:\n")
+                                ("r" "Research" entry (file+headline "~/Tresorit/notes/dphil.org" "Capture")
                                  "** %U %?\n")
                                 ("p" "Personal" entry (file+headline "~/Tresorit/notes/personal.org" "Capture")
-                                 "** %U %?\n"))))
+                                 "** %U %?\n"))
+        ))
 
 ;; export with pandoc
 (use-package ox-pandoc)
@@ -229,19 +260,31 @@
   :config
   (setq bibtex-completion-bibliography '("~/Tresorit/gms/library/zotero.bib")
         bibtex-completion-pdf-field "file"
-        bibtex-completion-notes-path "~/Tresorit/notes/papers/notes.org"
-        bibtex-completion-notes-template-one-file
-        "\n* ${author-or-editor} (${year}): ${title}\n  :PROPERTIES:\n  :Custom_ID: ${=key=}\n  :Date of notes: %(org-time-stamp-inactive)\n  :END:"
+        bibtex-completion-notes-path "~/Tresorit/notes/papers/"
+        bibtex-completion-notes-template-multiple-files
+        "* ${author-or-editor} (${journal}, ${year}): ${title}\n  :PROPERTIES:\n  :Custom_ID: ${=key=}\n  :Date of notes: %(org-time-stamp-inactive)\n  :END:\n\n"
         bibtex-completion-pdf-open-function  ;; open PDFs with system viewer
 	    (lambda (fpath)
 	      (call-process "evince" nil 0 nil fpath))))
   
 ;; org-ref
 (use-package org-ref
-  :config
-  (setq bibtex-completion-bibliography '("~/Tresorit/gms/library/zotero.bib")
-        bibtex-completion-library-path '("~/Tresorit/gms/library/pdfs/")
-	bibtex-completion-notes-path "~/Tresorit/notes/papers/notes.org"))
+  :init
+  (require 'bibtex)
+  (setq bibtex-autokey-year-length 4
+	bibtex-autokey-name-year-separator "-"
+	bibtex-autokey-year-title-separator "-"
+	bibtex-autokey-titleword-separator "-"
+	bibtex-autokey-titlewords 2
+	bibtex-autokey-titlewords-stretch 1
+	bibtex-autokey-titleword-length 5)
+  (define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
+  (define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
+  (define-key org-mode-map (kbd "s-[") 'org-ref-insert-link-hydra/body)
+  (require 'org-ref-ivy)
+  (require 'org-ref-arxiv)
+  (require 'org-ref-scopus)
+  (require 'org-ref-wos))
 
 (require 'org-ref-ivy)
 (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
@@ -250,12 +293,35 @@
       org-ref-insert-ref-function 'org-ref-insert-ref-link
       org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body)))
 
+(define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
+(define-key org-mode-map (kbd "s-[") 'org-ref-insert-link-hydra/body)
+
 ;; tramp
 (use-package tramp
   :defer t
   :config
   (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
 (put 'dired-find-alternate-file 'disabled nil)
+;; don't save Tramp passwords
+(setq password-cache nil)
+;; 2-factor authentication
+(customize-set-variable
+ 'tramp-password-prompt-regexp
+  (concat
+   "^.*"
+   (regexp-opt
+    '("passphrase" "Passphrase"
+      ;; English
+      "password" "Password"
+      ;; Deutsch
+      "passwort" "Passwort"
+      ;; Français
+      "mot de passe" "Mot de passe"
+      ;; BMRC
+      "First Factor"
+      "Second Factor")
+    t)
+   ".*:\0? *"))
 
 ;; flycheck
 (use-package flycheck
@@ -265,8 +331,9 @@
   (setq flycheck-global-modes '(not text-mode org-mode)))
 
 ;; libvterm
-(use-package vterm)
-
+;; (use-package vterm
+;;   :config
+;;   (setq vterm-shell "/bin/bash"))
 
 
 
